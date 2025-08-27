55bb1790eedbd5eb723805bc5f10b657040ad3d7
Chanser2001 <1870184217@qq.com>
2025-08-27 22:10:59 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+//
+//  Closed Solid Reconstruction Project
+//  Created by Shi Chen
+//	2025.08.27
+//	:)
+
+
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+#include <numeric> 
+
+
+
+int main(int argc, char* argv[])
+{	
+
+	return 0;
+}
